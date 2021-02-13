812241d0066b7e0089b4408341b2cb423f092a0d
israel Garica <israelgarciafigueroa>
2021-02-12 17:09:42 -0800

    Update CameraControlling.py

--- modules/CameraControlling.py
+++ modules/CameraControlling.py
@@ -3,19 +3,18 @@
     permit = False #semiphore to prevent setupt on the
 
 def CameraControlling():
-        if joy.Y(): # calls on xbox.py to check button press: 0 -> false, 1 -> true
-            print("camera button was pressed")
-            time.sleep(.4) #rebounding on button press
-            if camera == 0:
-                camera = 1
-                permit = True
-                return true
-                #cmd = "raspistill -t 1"
-
+    if joy.Y(): # calls on xbox.py to check button press: 0 -> false, 1 -> true
+        print("camera button was pressed")
+        time.sleep(.4) #rebounding on button press
+        if camera == 0:
+            camera = 1
+            permit = True
+            return true
+            #cmd = "raspistill -t 1"
             elif camera ==1:
-                camera =0
-                permit =True
-                return true
-        else :
-            permit = False
-            return False
+            camera =0
+            permit =True
+            return true
+    else :
+        permit = False
+        return False
