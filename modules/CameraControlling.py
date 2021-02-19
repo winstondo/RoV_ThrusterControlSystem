4cb93984b9d4f546f8747bd9ae5b50a9c60e07eb
israel Garica <israelgarciafigueroa>
2021-02-19 14:59:20 -0800

    debuggign

--- modules/CameraControlling.py
+++ modules/CameraControlling.py
@@ -1,15 +1,17 @@
 #joy = xbox.Joystick() # joy object for our xbox 360 camera
 import time
-camera = 1 # O will be cam a and 1 will be cam b
-permit = False #semiphore to prevent setupt on the
+global camera = 1 # O will be cam a and 1 will be cam b
+global permit = False #semiphore to prevent setupt on the
 
 def CamController(arg1):
+    global camera
+    global permit
     if arg1.Y(): # calls on xbox.py to check button press: 0 -> false, 1 -> true
         print("camera button was pressed")
         time.sleep(.4) #rebounding on button press
-        if CameraControlling.camera == 0:
-            CameraControlling.camera = 1
-            CameraControlling.permit = True
+        if camera == 0:
+            camera = 1
+            permit = True
             return true
             #cmd = "raspistill -t 1"
         elif CameraControlling.camera ==1:
